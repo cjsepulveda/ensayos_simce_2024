e0b749d75d6178cec2ebe6e69d5670ca4c9a9427
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-10-19 22:05:54 -0300

    grafico variaciones simce

--- src/app.py
+++ src/app.py
@@ -64,7 +64,9 @@ html.Div(
             options=[ 
                 {"label": "Nivel de Logro", "value": "level_score"},
                 {"label": "Habilidades", "value": "skill"},
-                {"label": "Promedio Habilidades", "value": "average"},
+                {"label": "Porcentaje Logro", "value": "average"},
+                {"label": "Puntajes", "value": "score"},
+                {"label": "Variaciones Puntajes", "value": "var_score"}
                ],
             value= 'level_score',
             clearable=False,
@@ -102,7 +104,7 @@ def update_charts(nivel,test,asig):
           n='2° Medio'
 
     if asig == 'mat':
-          df01 = pd.read_excel(DATA_PATH.joinpath('data_dia_2024.xlsx'), sheet_name='data_dia_mat')
+          df01 = pd.read_excel(DATA_PATH.joinpath('data_ensayo_simce_2024.xlsx'), sheet_name='data_simce_mat')
           mask01=df01[df01['NIVEL'] == nivel]
           a ='Matemáticas'
           count_skill = [0,1,2,3]
@@ -110,10 +112,12 @@ def update_charts(nivel,test,asig):
           colors_skill=['#00308F','#03C03C','#ffbf00','#c91b00']
           graph_y_axes_SKILL=[mask01['num'],mask01['alg'],mask01['geo'],mask01['dat']]
           graph_y_axes_average=mask01['prom_mat']
+          graph_y_axes_score=mask01['punt_mat']
           color_avr='#007fd2'
+          color_score='#FFD966'
 
     elif asig == 'len':
-          df01 = pd.read_excel(DATA_PATH.joinpath('data_dia_2024.xlsx'), sheet_name='data_dia_len')
+          df01 = pd.read_excel(DATA_PATH.joinpath('data_ensayo_simce_2024.xlsx'), sheet_name='data_simce_len')
           mask01=df01[df01['NIVEL'] == nivel]
           a ='Lenguaje'
           count_skill = [0,1,2]
@@ -121,18 +125,26 @@ def update_charts(nivel,test,asig):
           colors_skill=['#00308F','#03C03C','#ffbf00']
           graph_y_axes_SKILL=[mask01['loc'],mask01['int'],mask01['ref']]
           graph_y_axes_average=mask01['prom_len']
-          color_avr='#ffaf2b '
+          graph_y_axes_score=mask01['punt_len']
+          color_avr='#ffaf2b'
+          color_score='#0147EB'
 
     
     # Parámetros constantes para ambas asignaturas, petenecientes al descriptor NIVEL de LOGRO
     count_level=[0,1,2]
-    name_level=['Nivel I','Nivel II','Nivel III']
-    graph_y_axes_LEVEL=[mask01['NIVEL I'], mask01['NIVEL II'], mask01['NIVEL III']]
+    name_level=['INSUFICIENTE','ELEMENTAL','ADECUADO']
+    graph_y_axes_LEVEL=[mask01['INSUFICIENTE'], mask01['ELEMENTAL'], mask01['ADECUADO']]
     colors_level=['#062c80','#0e6ac7','#4fb9fc']
     
-    # Parámetros constantes para el gráfico, TITULO, y eje X con múltiples valores
-    new_hovertemplate = 'Rendimiento: %{y:.0%}'+'<br>Curso: %{x[0]}<br>'+'Etapa: %{x[1]}'
-    graph_x_axes = [mask01['CURSO'], mask01['Etapa']]
+    # Parámetros constantes para el gráfico, leyenda barras, y eje X con múltiples valores
+
+    if test =='score':
+         new_hovertemplate = 'Rendimiento: %{y:.1f}'+'<br>Curso: %{x[0]}<br>'+'Etapa: %{x[1]}'
+         graph_x_axes = [mask01['CURSO'],mask01['Etapa']]
+    
+    else:
+        new_hovertemplate = 'Rendimiento: %{y:.0%}'+'<br>Curso: %{x[0]}<br>'+'Etapa: %{x[1]}'
+        graph_x_axes = [mask01['CURSO'],mask01['Etapa']]
 
     #print(mask01.iloc[:,1])
            
@@ -168,10 +180,36 @@ def update_charts(nivel,test,asig):
                             hovertemplate = new_hovertemplate)
                         
             trace01.update_layout(barmode="group",  template='simple_white')
-            b ='Promedio Habilidades'
+            b ='Porcentaje Logro'
     
+    elif test =='score': #Gráfica para PUNTAJES SIMCE
+
+            trace01.add_bar( x=graph_x_axes, y=graph_y_axes_score, 
+                            name='PUNTAJES SIMCE', 
+                            marker_color=color_score,
+                            hovertemplate = new_hovertemplate)
+                        
+            trace01.update_layout(barmode="group",  template='simple_white')
+            b='Puntajes'
+
+    elif test =='var_score': #Gráfica VARIACIONES PUNTAJES SIMCE
+            df02 = pd.read_excel(DATA_PATH.joinpath('data_ensayo_simce_2024.xlsx'), sheet_name='var')
+            graph_x_axes = [df02['NIVEL'],df02['ASIGNATURA']]
+            graph_y_axes_score_var=df02['VALOR']
+
+            trace01.add_bar( x=graph_x_axes, y=graph_y_axes_score_var, 
+                            name='VARIACIONES PUNTAJES SIMCE', 
+                            marker_color=color_score,
+                            hovertemplate = new_hovertemplate)
+                        
+            trace01.update_layout(barmode="group", template='simple_white')
+            b='Puntajes'
+            
+    
+    # actualizaciones LAYOUT gráfico
+
     trace01.update_layout(
-    title_text=f"Rendimiento DIA: {b}, {a} {n}",
+    title_text=f"ENSAYOS SIMCE: {b}, {a} {n}",
     title_font_family='Consolas',
     title_font_weight=1000,
     legend_font_family='Consolas',
@@ -180,22 +218,44 @@ def update_charts(nivel,test,asig):
     title_x= 0.0,
     title_font_size=25,
     legend_title_text='Descriptor',
-    autosize=False,
+    autosize=True,
+    bargap=0.5,
     legend=dict(
-                orientation="v",
+                orientation="h",
                 yanchor="bottom",
-                y=0.90,
+                y=1.0,
                 xanchor="right",
-                x=1.07),
+                x=0.7),
     width=1000,
     height=400,
     margin=dict(l=0, r=0, b=50, t=80, pad=0),
        
         )
-    trace01.update_yaxes(tickformat='.0%', tickfont_family='Consolas', tickfont_size=15, tickfont_weight=1000)
-    trace01.update_xaxes(tickfont_family='Consolas', tickfont_size=15, tickfont_weight=1000)
+
+    if test == 'score': #opcion grafico numérico, porcentaje
+
+        trace01.update_yaxes(tickformat='.0f', tickfont_family='Consolas', tickfont_size=15, tickfont_weight=1000)
     
-    
+    elif test == 'var_score':
+
+        trace01.update_yaxes(
+            tickformat='.0%', 
+            tickfont_family='Consolas', 
+            tickfont_size=15, 
+            tickfont_weight=1000,
+            zeroline=True,
+            zerolinecolor='black'
+            )
+        trace01.update_xaxes(
+            tickfont_family='Consolas', 
+            tickfont_size=15, 
+            tickfont_weight=1000,
+            )
+      
+    else:
+        trace01.update_yaxes(tickformat='.0%', tickfont_family='Consolas', tickfont_size=15, tickfont_weight=1000)
+        trace01.update_xaxes(tickfont_family='Consolas', tickfont_size=15, tickfont_weight=1000)
+       
     
     new_trace01 = [dcc.Graph(figure=trace01, config={"displayModeBar": False}, className="card")]
    
